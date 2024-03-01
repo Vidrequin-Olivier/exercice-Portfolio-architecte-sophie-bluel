76b66e7a34db4cc4406d320234838a4c48cfd5f9
Vidrequin Olivier <vidrequin.olivier@gmail.com>
2024-03-01 13:19:15 +0100

    amélioration de la lisibilité de la fonction fetchWorks() correctif du nom de sortBar() en sortingBar() correctif du nom et completion la fonction sortingButtonSelector() avancement sur la fonction displaySelectedCards(cards) mises à jour et implémentation du CSS

--- FrontEnd/assets/index.js
+++ FrontEnd/assets/index.js
@@ -5,60 +5,61 @@ async function fetchWorks() {
         try {
             const apiAddress = "http://localhost:5678/api";
             const response = await fetch(apiAddress + "/works");
-            if (!response.ok) {
-                alert("La requête API n'a pas pu aboutir.");
-                throw new Error("La requête API n'a pas pu aboutir.");
-            } else {
+            if (response.ok) {
                 data = await response.json();
                 const worksStringified = JSON.stringify(data);
                 window.sessionStorage.setItem("works", worksStringified);
+            } else {
+                alert("La requête API n'a pas pu aboutir.");
+                throw new Error("La requête API n'a pas pu aboutir.");
             };
         } catch (error) {
             console.error("Une erreur est survenue: ", error.message);
         };
     };
+    displaySelectedCards(createCards())
 };
 
 // Display the sorting bar
-function sortBar() {
+function sortingBar() {
     const parentElement = document.getElementById("portfolio");
     const h2 = parentElement.getElementsByTagName("h2");
     const previousTag = h2[0];
-    const sortBar = document.createElement("div");
-    sortBar.className = "sortBar";
-    sortBar.innerHTML = `
+    const sortingBar = document.createElement("div");
+    sortingBar.className = "sortingBar";
+    sortingBar.innerHTML = `
                             <button class="button">Tous</button>
                             <button class="button">Objets</button>
                             <button class="button">Appartements</button>
                             <button class="button">Hôtels & restaurants</button>
                         `;
-    parentElement.insertBefore(sortBar, previousTag.nextSibling);
+    parentElement.insertBefore(sortingBar, previousTag.nextSibling);
 };
 
-// function sortBar() {
+// function sortingBar() {
 //     const parentElement = document.getElementById("portfolio");
-//     const sortBar = document.createElement("div");
-//     sortBar.className = "sortBar";
-//     sortBar.innerHTML = `
+//     const sortingBar = document.createElement("div");
+//     sortingBar.className = "sortingBar";
+//     sortingBar.innerHTML = `
 //                             <button class="button">Tous</button>
 //                             <button class="button">Objets</button>
 //                             <button class="button">Appartements</button>
 //                             <button class="button">Hôtels & restaurants</button>
 //                         `;
-//     parentElement.appendChild(sortBar);
+//     parentElement.appendChild(sortingBar);
 // };
 
-// Set the selected sorting button on load and upon click.
-function sortButtonSelected() {
-    const buttonsList = document.querySelectorAll("button");
-    let buttonSelected = buttonsList[0];
-    buttonsList.forEach((el) => {
-        el.addEventListener("click", (e) => {
-            let isClicked = buttonsList[e];
-            console.log(isClicked);
+// Set the selected sorting button on page load and upon click.
+function sortingButtonSelector() {
+    const sortingBar = document.querySelector(".sortingBar");
+    const buttonsList = sortingBar.querySelectorAll("button");
+    buttonsList[0].classList.add("buttonSelected");
+    buttonsList.forEach(el => {
+        el.addEventListener("click", () => {
+            buttonsList.forEach(e => e.classList.remove("buttonSelected"));
+            el.classList.add("buttonSelected")
         });
     });
-    buttonSelected.className += " buttonSelected"
 };
 
 // Retrieve data from session storage and create 'card' elements.
@@ -66,6 +67,7 @@ function createCards() {
     const worksList = JSON.parse(window.sessionStorage.getItem("works"));
     let cards = [];
     for (let i = 0; i < worksList.length; i++) {
+        // remplacer cards += par cards.push() et corriger l'affichage
         cards += `  
                     <article class="categoryId:${worksList[i].categoryId}" >
                         <figure>
@@ -78,18 +80,24 @@ function createCards() {
     return cards;
 };
 
-//  Display the cards based on the selected sort button.
+// Display the cards based on the selected sort button.
 function displaySelectedCards(cards) {
     const gallery = document.querySelector(".gallery");
+    // let displayedCards = [];
+    // cards.forEach(el => {
+    //     if(el.classList.contains("buttonSelected")) {
+    //         displayedCards.push(el);
+    //     };
+    // });
+    // gallery.innerHTML = displayedCards;
     gallery.innerHTML = cards;
 };
 
 // Is it really useful to explain it ?
 function mainFunction() {
     fetchWorks();
-    sortBar();
-    sortButtonSelected();
-    displaySelectedCards(createCards())
+    sortingBar();
+    sortingButtonSelector();
 };
 
 mainFunction();
